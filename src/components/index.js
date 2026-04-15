b4f70561e6bd66c67299de7b5afa3f86e1773d88
fcabrerab10 <fernando.cabrera@acteck.com>
2026-04-14 18:18:26 -0600

    feat: components barrel export

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,1 @@
-export { Semaforo } from './Semaforo';
-export { KPICard } from './KPICard';
-export { CardHeader } from './CardHeader';
-export { TarjetaPendientes } from './TarjetaPendientes';
-export { TarjetaPagos } from './TarjetaPagos';
-export { TarjetaPromociones } from './TarjetaPromociones';
-export { TarjetaMinuta } from './TarjetaMinuta';
-export { BarraCuota } from './BarraCuota';
 export { default as Sidebar } from './Sidebar';
